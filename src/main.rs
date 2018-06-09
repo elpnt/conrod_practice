dc55982c1c38da65b3e6674e24208f93ba636235
velvetops <quantumular@gmail.com>
2018-06-09 13:04:57 +0900

    hello world

---
+++ src/main.rs
@@ -0,0 +1,98 @@
+#[macro_use] extern crate conrod;
+
+use conrod::{widget, Colorable, Positionable, Widget};
+use conrod::backend::glium::glium::{self, Surface};
+
+const WIDTH: u32 = 400;
+const HEIGHT: u32 = 200;
+
+fn main() {
+    // Build the window
+    let mut events_loop = glium::glutin::EventsLoop::new();
+    let window = glium::glutin::WindowBuilder::new()
+        .with_title("Hello Conrod")
+        .with_dimensions(WIDTH, HEIGHT);
+    let context = glium::glutin::ContextBuilder::new()
+        .with_vsync(true)
+        .with_multisampling(4);
+    let display = glium::Display::new(window, context, &events_loop).unwrap();
+
+    // Construct Ui
+    let mut ui = conrod::UiBuilder::new([WIDTH as f64, HEIGHT as f64]).build();
+
+    // Generate the widget identifiers
+    widget_ids!(struct Ids { text });
+    let ids = Ids::new(ui.widget_id_generator());
+
+    // Add a `Font`
+    const FONT_PATH: &'static str =
+        concat!(env!("CARGO_MANIFEST_DIR"), "/monaco.ttf");
+    ui.fonts.insert_from_file(FONT_PATH).unwrap();
+
+    let mut renderer = conrod::backend::glium::Renderer::new(&display).unwrap();
+
+    let image_map = conrod::image::Map::<glium::texture::Texture2d>::new();
+
+    let mut events = Vec::new();
+
+    'render: loop {
+        events.clear();
+
+        // Get all the new events since the last frame
+        events_loop.poll_events(|event| {events.push(event);});
+
+        if events.is_empty() {
+            events_loop.run_forever(|event| {
+                events.push(event);
+                glium::glutin::ControlFlow::Break
+            });
+        }
+
+        for event in events.drain(..) {
+            // Break from the loop upon `Escape` or closed window
+            match event.clone() {
+                glium::glutin::Event::WindowEvent {event, ..} => {
+                    match event {
+                        glium::glutin::WindowEvent::Closed |
+                        glium::glutin::WindowEvent::KeyboardInput {
+                            input: glium::glutin::KeyboardInput {
+                                virtual_keycode: Some(glium::glutin::VirtualKeyCode::Escape),
+                                ..
+                            },
+                            ..
+                        } => break 'render,
+                        _ => (),
+                    }
+                }
+                _ => (),
+            };
+
+            // Use the `winit` backend feature
+            let input = match conrod::backend::winit::convert_event(event, &display) {
+                None => continue,
+                Some(input) => input,
+            };
+
+            // Handle the input with the `Ui`
+            ui.handle_event(input);
+
+            // Set the widgets
+            let ui = &mut ui.set_widgets();
+
+            // "Hello, World!" in the middle of the screen
+            widget::Text::new("Hello, World")
+                .middle_of(ui.window)
+                .color(conrod::color::WHITE)
+                .font_size(32)
+                .set(ids.text, ui);
+        }
+        
+        if let Some(primitives) = ui.draw_if_changed() {
+            renderer.fill(&display, primitives, &image_map);
+            let mut target = display.draw();
+            target.clear_color(0., 0., 0., 1.);
+            renderer.draw(&display, &mut target, &image_map).unwrap();
+            target.finish().unwrap();
+        }
+    }
+}
