b98c4a3ba7532c49399e90fb15e2335e6340585a
velvetops <quantumular@gmail.com>
2018-06-12 13:47:11 +0900

    a

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,25 @@
-extern crate glium;
-extern crate winit;
 extern crate conrod;
 
-use glium::glutin;
-use winit::{ControlFlow, Event, WindowEvent};
-
+#[allow(unused_variables)]
 fn main() {
+    // use conrod;
+    use conrod::backend::glium::glium;
+    // use conrod::backend::glium::glium::Surface;
+    use conrod::backend::glium::glium::glutin::{ControlFlow, Event, WindowEvent};
+
     // Build the window
-    let mut events_loop = glutin::EventsLoop::new();
-    let window = glutin::WindowBuilder::new()
+    let mut events_loop = glium::glutin::EventsLoop::new();
+    let window = glium::glutin::WindowBuilder::new()
         .with_title("Hello world")
         .with_dimensions(400, 300);
-    let context = glutin::ContextBuilder::new();
-    let _glwindow = glutin::GlWindow::new(window, context, &events_loop).unwrap();
-    
+    let context = glium::glutin::ContextBuilder::new();
+    let display = glium::Display::new(window, context, &events_loop).unwrap();
+
     events_loop.run_forever(|event| {
         match event {
-            Event::WindowEvent {event: WindowEvent::Closed, ..} => {
-                println!("The window was closed ; stopping");
-                ControlFlow::Break
-            },
+            Event::WindowEvent {event: WindowEvent::Closed, ..} => ControlFlow::Break,
             _ => ControlFlow::Continue,
         }
     });
+    
 }
